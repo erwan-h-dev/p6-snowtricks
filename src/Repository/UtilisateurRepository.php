ee1d786e9359a4f6c406aab765be16f16372ed8a
erwan-h-dev <erwan.h.dev@gmail.com>
2023-04-20 11:00:06 +0000

    mise en place du systeme de réinitialisation de mot de passe #6

--- src/Repository/UtilisateurRepository.php
+++ src/Repository/UtilisateurRepository.php
@@ -39,28 +39,13 @@ class UtilisateurRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Utilisateur[] Returns an array of Utilisateur objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('u.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Utilisateur
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+   public function findOneByUsername($username): ?Utilisateur
+   {
+       return $this->createQueryBuilder('u')
+           ->andWhere('u.username = :username')
+           ->setParameter('username', $username)
+           ->getQuery()
+           ->getOneOrNullResult()
+       ;
+   }
 }
